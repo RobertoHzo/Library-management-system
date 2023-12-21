1bc5fab1ecbde1f314f47c85c0bdf7138e32a318
Roberto Hernandez <125925089+RobertoHzo@users.noreply.github.com>
2023-12-20 20:12:38 -0600

    Delete unused directories
    Resolved coflicts

--- Library7/Data/DBContext1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Library7.Data
-{
-    public class DBContext1
-    {
-        public DBContext1(string valor) => Valor = valor;
-        public string Valor { get; set; }
-    }
-}
--- Library7/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Library7.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
